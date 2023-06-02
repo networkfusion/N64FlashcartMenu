cb7f4cffc6adf27d18a2323bd65e2ccf6427fff6
Robin Jones <networkfusion@users.noreply.github.com>
2023-06-02 21:01:37 +0100

    Improve readme

--- remotedeploy.sh
+++ remotedeploy.sh
@@ -4,14 +4,14 @@ set -e
 
 REMOTE="--remote host.docker.internal:9064"
 
-sc64deployer $REMOTE upload N64FlashcartMenu.z64
+sc64deployer $REMOTE upload ./output/N64FlashcartMenu.z64
 
-echo .
-echo .
+echo
+echo
 # Toggle the power of the N64 to boot the ROM.
 echo !!! Now toggle power to the N64 !!!
-echo .
-echo .
+echo
+echo
 
 if [ "$1" = "-d" ]; then
     sc64deployer $REMOTE debug
