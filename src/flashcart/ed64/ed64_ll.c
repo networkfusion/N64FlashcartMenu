05675e8043fca6e421c3abf5b4746f8b93ded2f4
Robin Jones <networkfusion@users.noreply.github.com>
2023-10-28 19:36:16 +0100

    Start adding device variants

--- src/flashcart/ed64/ed64_ll.c
+++ src/flashcart/ed64/ed64_ll.c
@@ -25,7 +25,7 @@ typedef enum {
 
 } ed64_registers_t;
 
-// #define DEVICE_VARIANT_MASK         (0xF000)
+#define ED64_DEVICE_VARIANT_MASK         (0xF000)
 
 void pi_initialize (void);
 void pi_initialize_sram (void);
