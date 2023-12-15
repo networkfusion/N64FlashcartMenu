4e651a26ba1b6f3ad8b110164f6fa6dd9ae20e83
networkfusion <networkfusion@users.noreply.github.com>
2023-12-15 17:38:03 +0000

    deploy: fc1aa313835d17589395a50b44ad12ca5ad5f58f

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,12 +1,4 @@
 var searchData=
 [
-  ['rom_5fheader_5ft_0',['rom_header_t',['../structrom__header__t.html',1,'']]],
-  ['rom_5fheader_5ft_2e_5f_5funnamed9_5f_5f_1',['rom_header_t.__unnamed9__',['../unionrom__header__t_8____unnamed9____.html',1,'']]],
-  ['rom_5fheader_5ft_2e_5f_5funnamed9_5f_5f_2e_5f_5funnamed11_5f_5f_2',['rom_header_t.__unnamed9__.__unnamed11__',['../structrom__header__t_8____unnamed9_____8____unnamed11____.html',1,'']]],
-  ['rom_5fheader_5ft_2elibultra_3',['rom_header_t.libultra',['../structrom__header__t_8libultra.html',1,'']]],
-  ['rom_5finfo_5ft_4',['rom_info_t',['../rom__info_8h.html#structrom__info__t',1,'']]],
-  ['rom_5finfo_5ft_2e_5f_5funnamed18_5f_5f_5',['rom_info_t.__unnamed18__',['../rom__info_8h.html#unionrom__info__t_8____unnamed18____',1,'']]],
-  ['rom_5finfo_5ft_2e_5f_5funnamed18_5f_5f_2e_5f_5funnamed21_5f_5f_6',['rom_info_t.__unnamed18__.__unnamed21__',['../rom__info_8h.html#structrom__info__t_8____unnamed18_____8____unnamed21____',1,'']]],
-  ['rom_5finfo_5ft_2efeatures_7',['rom_info_t.features',['../rom__info_8h.html#structrom__info__t_8features',1,'']]],
-  ['rom_5finfo_5ft_2elibultra_8',['rom_info_t.libultra',['../rom__info_8h.html#structrom__info__t_8libultra',1,'']]]
+  ['usb_5fcomm_5fcommand_5ft_0',['usb_comm_command_t',['../structusb__comm__command__t.html',1,'']]]
 ];
--- search/classes_c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['usb_5fcomm_5fcommand_5ft_0',['usb_comm_command_t',['../structusb__comm__command__t.html',1,'']]]
-];
