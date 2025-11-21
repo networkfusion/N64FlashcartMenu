774edb84f67ea8bf57e28cd6666af05ab5f93945
networkfusion <networkfusion@users.noreply.github.com>
2025-11-21 22:04:09 +0000

    deploy: 33995e4b7191e6c7f7be6653e379c1c3903ae527

--- search/all_f.js
+++ search/all_f.js
@@ -1,11 +1,17 @@
 var searchData=
 [
-  ['game_20art_20images_0',['Game Art Images',['../md_docs_219__gamepak__boxart.html#autotoc_md66',1,'']]],
-  ['game_5fcode_1',['game_code',['../rom__info_8h.html#a4f17ea269979c808050cd0de5aa32afa',1,'rom_info_t.__unnamed22__']]],
-  ['gameshark_20etc_2',['Cheats (Gameshark, etc.)',['../md_docs_213__datel__cheats.html#autotoc_md58',1,'']]],
-  ['general_3',['General',['../md_docs_200__index.html#autotoc_md28',1,'']]],
-  ['generate_20documentation_4',['Generate documentation',['../md_docs_299__developer__guide.html#autotoc_md107',1,'']]],
-  ['generated_20docs_20in_20the_20dev_20container_5',['Test generated docs in the dev-container',['../md_docs_299__developer__guide.html#autotoc_md108',1,'']]],
-  ['get_5ffirmware_5fversion_6',['get_firmware_version',['../flashcart_8h.html#af5b584e89ac94844a234ef9ae56dfff3',1,'flashcart_t']]],
-  ['guide_7',['Guide',['../md_docs_299__developer__guide.html#autotoc_md97',1,'Developer Guide'],['../md_docs_200__index.html#autotoc_md27',1,'N64FlashcartMenu User Guide']]]
+  ['game_20art_20images_0',['Game Art Images',['../md_docs_219__gamepak__boxart.html#autotoc_md76',1,'']]],
+  ['game_20art_20images_1',['Itterating through game art images',['../md_docs_219__gamepak__boxart.html#autotoc_md79',1,'']]],
+  ['game_5fcode_2',['game_code',['../rom__info_8h.html#a4f17ea269979c808050cd0de5aa32afa',1,'rom_info_t.__unnamed22__']]],
+  ['gamecode_3',['gamecode',['../cpakfs__utils_8h.html#ae9da9578e7b8ae989a881e322a82ceed',1,'cpakfs_path_strings_t']]],
+  ['gameshark_20etc_4',['Cheats (Gameshark, etc.)',['../md_docs_213__datel__cheats.html#autotoc_md61',1,'']]],
+  ['general_5',['General',['../md_docs_200__index.html#autotoc_md31',1,'']]],
+  ['generate_20documentation_6',['Generate documentation',['../md_docs_299__developer__guide.html#autotoc_md134',1,'']]],
+  ['generate_5fenabled_5fcheats_5farray_7',['generate_enabled_cheats_array',['../datel__codes_8h.html#ae74416630fc80ad6f62a02a535c09b7c',1,'datel_codes.c']]],
+  ['generated_20docs_20in_20the_20dev_20container_8',['Test generated docs in the dev-container',['../md_docs_299__developer__guide.html#autotoc_md135',1,'']]],
+  ['get_5fblock_5fsize_5ffrom_5ffs_5fpath_9',['get_block_size_from_fs_path',['../cpakfs__utils_8c.html#ab91af506e07a9d9a262e599df0e03b2b',1,'get_block_size_from_fs_path(const char *filename_cpak):&#160;cpakfs_utils.c'],['../cpakfs__utils_8h.html#ab91af506e07a9d9a262e599df0e03b2b',1,'get_block_size_from_fs_path(const char *filename_cpak):&#160;cpakfs_utils.c']]],
+  ['get_5fcheat_5fcodes_10',['get_cheat_codes',['../datel__codes_8h.html#ae8bdc386f52913f11c7f215dac7e69c6',1,'datel_codes.c']]],
+  ['get_5ffile_5fsize_5ffrom_5ffs_5fpath_11',['get_file_size_from_fs_path',['../cpakfs__utils_8c.html#aab60c976955f46d4edcca087af0e2717',1,'get_file_size_from_fs_path(const char *filename_cpak):&#160;cpakfs_utils.c'],['../cpakfs__utils_8h.html#aab60c976955f46d4edcca087af0e2717',1,'get_file_size_from_fs_path(const char *filename_cpak):&#160;cpakfs_utils.c']]],
+  ['get_5ffirmware_5fversion_12',['get_firmware_version',['../flashcart_8h.html#af5b584e89ac94844a234ef9ae56dfff3',1,'flashcart_t']]],
+  ['guide_13',['Guide',['../md_docs_299__developer__guide.html#autotoc_md118',1,'Developer Guide'],['../md_docs_200__index.html#autotoc_md30',1,'N64FlashcartMenu User Guide']]]
 ];
